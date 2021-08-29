374ab8c4be5ed05e3bca0eb7c81e57aa85b5f25c
kiminkim724 <55262612+kiminkim724@users.noreply.github.com>
2021-08-29 17:43:55 -0400

    Added User account and login changes

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import dotenv from 'dotenv';
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
 
-import postRoutes from "./routes/posts.js"
+import postRoutes from "./routes/posts.js";
+import userRoutes from "./routes/users.js";
 
 const app = express();
 dotenv.config();
@@ -13,16 +14,23 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-app.use('/posts', postRoutes);
+app.use("/posts", postRoutes);
+app.use("/user", userRoutes);
 
-app.get('/', (req, res) => {
-    res.send('Hello to Memories API');
+app.get("/", (req, res) => {
+  res.send("Hello to Memories API");
 });
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => app.listen(PORT, () => console.log(`Server is running port: ${PORT}`)))
-    .catch((error) => console.log(error.message));
+mongoose
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server is running port: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
 
-mongoose.set('useFindAndModify', false);
+mongoose.set("useFindAndModify", false);
